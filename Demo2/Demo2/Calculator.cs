bfa876e6c9722abbf595e27930c869f6b5b20f85
entropy-sumit <sumitr600@gmail.com>
2021-10-29 17:47:24 +0530

    addition calculator

---
+++ Demo2/Demo2/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo2
+{
+    public class Calculator
+    {
+        int num1;
+        int num2;
+        int totalvalue;
+
+        public static int isSum(int num1, int num2)
+        {
+
+            return totalvalue = num1 + num2;
+
+        }
+    
+    }
+
+
+}
---
+++ Demo2/Demo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo2
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Calculator objCalculator = new Calculator();
+            Console.WriteLine("totalvalue="+ objCalculator.isSum(20, 20));
+            Console.ReadKey();
+
+        }
+    
+    }
+
+
+}
