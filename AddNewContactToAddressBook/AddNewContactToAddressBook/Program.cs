929a33d67952ce427e5ac13abfdd89675b506dd4
entropy-sumit <sumitr600@gmail.com>
2021-11-16 17:15:05 +0530

    day9uc2

---
+++ AddNewContactToAddressBook/AddNewContactToAddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddNewContactToAddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConatctPerson contactperson = new ConatctPerson();
+            contactperson.displayContact();
+
+        }
+    }
+}
