6176f2463f86226415781c28c25f33c99a83c112
entropy-sumit <sumitr600@gmail.com>
2021-12-02 12:59:07 +0530

    Day22 Reflection of user registration

---
+++ ReflectionOfRegistration/ReflectionOfRegistration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionOfRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //for assembly
+            Assembly assembly = typeof(int).Assembly;
+            Console.WriteLine(assembly);
+
+            Type type = typeof(System.String);
+            Console.WriteLine(type.Assembly);
+
+
+            //For method
+            Type t = typeof(UserRegistration);
+            MethodInfo[] mi = t.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (MethodInfo m in mi)
+            {
+                Console.WriteLine(m);
+
+            }
+        }
+    }
+}
---
+++ ReflectionOfRegistration/ReflectionOfRegistration/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReflectionOfRegistration
+{
+     class UserRegistration
+    {
+        public static string First_nameuc1 = "^[A-Z]{3}";
+
+
+
+        public bool validating(string name)
+        {
+            return Regex.IsMatch(name, First_nameuc1);
+        }
+
+    }
+    
+}
